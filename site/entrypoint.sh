74c4f39850d1415612d6a1e1abd8222ed8e32b9b
Jeremiah Boby <mail@jeremiahboby.me>
2020-05-06 13:33:52 +0100

    Initial Commit
    Add boilerplate Django project and compose configuration.

---
+++ site/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/dumb-init /bin/sh
+
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+cd /usr/src/app/anonchat
+python manage.py collectstatic --no-input
+
+exec "$@"
